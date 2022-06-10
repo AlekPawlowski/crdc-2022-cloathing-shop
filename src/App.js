c3011a8f1b3598df67c98baf92edf2ad8ef46bb4
Aleksander Pawłowski <apawlowski@fly.pl>
2022-06-10 16:32:14 +0200

    create components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,10 @@
-import './categories.styles.scss';
+
+import CategoryContainer from './components/category-container/category-container.component.jsx';
 
 const App = () => {
-    const categories = [
-        {
-            id: 1,
-            title: 'Hats'
-        },
-        {
-            id: 2,
-            title: 'Jackets'
-        },
-        {
-            id: 3,
-            title: 'Sneakers'
-        },
-        {
-            id: 4,
-            title: 'Womans'
-        },
-        {
-            id: 5,
-            title: 'Mens'
-        },
-
-    ];
     return (
-        <div className='categories-container'>
-            {categories.map(({ title }, key) => 
-                <div key={key} className="category-container">
-                    {/* <img src="" alt="" /> */}
-                    <div className="category-body-container">
-                        <h2>{title}</h2>
-                        <p>Shop Now</p>
-                    </div>
-                </div>
-            )}
+        <div>
+            <CategoryContainer />
         </div>
     );
 }
