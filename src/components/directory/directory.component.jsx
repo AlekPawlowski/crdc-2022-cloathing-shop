4efb5abfebb8c219f7f16d19a41987c82e06b505
Aleksander Pawłowski <apawlowski@fly.pl>
2022-06-13 16:36:51 +0200

    add navigation bar

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,14 +1,13 @@
-import './directory.styles.scss';
-import CategoryItem from '../category-item/category-item.component';
+import "./directory.styles.scss";
+import CategoryItem from "../category-item/category-item.component";
 
-const Directory = ({categories}) =>{
-    
-    return(
+const Directory = ({ categories }) => {
+    return (
         <div className="directory-container">
-            {categories.map((category) =>
+            {categories.map((category) => (
                 <CategoryItem key={category.id} category={category} />
-            )}
+            ))}
         </div>
-    )
-}
+    );
+};
 export default Directory;
