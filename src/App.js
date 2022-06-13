06bfde28747bebd00ab5297266ab0b07ff1a3e13
Aleksander Pawłowski <apawlowski@fly.pl>
2022-06-13 16:57:37 +0200

    navigation style

--- src/App.js
+++ src/App.js
@@ -1,18 +1,7 @@
 import './app.styles.scss';
-import { Routes, Route, Outlet } from 'react-router-dom';
-
+import { Routes, Route } from 'react-router-dom';
+import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
-
-const Navigation = () =>{
-    return (
-        <div>
-            <div>
-                <h1>I am the navigation</h1>
-            </div>
-            <Outlet />
-        </div>
-    )
-}
 
 const Shop = () => {
     return (
