894ed6398ac4028149657e1424d90db54846cec0
Aleksander Pawłowski <apawlowski@fly.pl>
2022-06-14 11:44:19 +0200

    fix up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import './app.styles.scss';
 import { Routes, Route } from 'react-router-dom';
-import Navigation from './routes/navigation/navigation.component';
+
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop = () => {
-    return (
-        <div>
-            <h1>Shop componetn</h1>
-        </div>
-    )
-}
+    return <h1>I am the shop page</h1>;
+};
 
 const App = () => {
     return (
         <Routes>
             <Route path='/' element={<Navigation />}>
-                <Route index element={<Home />}/>
-                <Route path='shop' element={<Shop />}/>
+                <Route index element={<Home />} />
+                <Route path='shop' element={<Shop />} />
+                <Route path='sign-in' element={<SignIn />} />
             </Route>
         </Routes>
-    )
-}
+    );
+};
 
 export default App;
