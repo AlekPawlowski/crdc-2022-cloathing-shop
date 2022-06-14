67372df807e9ca1336bd0299d1f2ad1e990421e6
Aleksander Pawłowski <apawlowski@fly.pl>
2022-06-14 11:38:36 +0200

    firebase?

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider
+} from "firebase/auth";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDn8vSZ32zRLNGZm_9XkRkmFg8acGjHc5I",
+    authDomain: "crwn-clothing-db-ac62a.firebaseapp.com",
+    projectId: "crwn-clothing-db-ac62a",
+    storageBucket: "crwn-clothing-db-ac62a.appspot.com",
+    messagingSenderId: "218448381006",
+    appId: "1:218448381006:web:54a64bb5a90f2ebdc1ef1f"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+        prompt: "select_account"
+})
+
+export const auth = getAuth();
+export const singInWithGooglePopup = () => signInWithPopup(auth, provider);
