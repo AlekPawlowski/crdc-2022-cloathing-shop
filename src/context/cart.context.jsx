9b8ac153986b4d5c150de134ee9c9d3b576d2453
Aleksander Pawłowski <apawlowski@fly.pl>
2022-06-30 16:56:32 +0200

    cart item

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -19,17 +19,26 @@ export const CartContext = createContext({
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
+    cartTotalItems: 0,
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
     const [cartItems, setCartItem] = useState([]);
+    const [cartTotalItems, setCartTotalItems] = useState(0);
 
     const addItemToCart = (productToAdd) => {
         setCartItem(addCartItem(cartItems, productToAdd));
     };
 
-    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
+    const countCartTotal = () => {
+        let counter = 0;
+        cartItems.map(item=>{
+            console.log(item);
+        })
+    }
+
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems, cartTotalItems, countCartTotal };
 
     return (
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
