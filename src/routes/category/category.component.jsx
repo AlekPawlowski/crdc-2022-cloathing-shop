effebd2ba7c4e311d057e87f891e5ed17e39152d
Aleksander Pawłowski <apawlowski@fly.pl>
2022-07-06 14:37:08 +0200

    styles add

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -15,13 +15,17 @@ const Category = () => {
     useEffect(() => {
         setProducts(categoriesMap[category]);
     }, [category, categoriesMap]);
-
+    console.log(products, category);
     return (
-        <div className="category-container inner_category_container">
-            {products && products.map((product) => (
-                <ProductCard key={product.id} product={product} />
-            ))}
-        </div>
+        <>
+            <h2 className='category-title'>{category.toUpperCase()}</h2>
+            <div className="category-container inner_category_container">
+                {products &&
+                    products.map((product) => (
+                        <ProductCard key={product.id} product={product} />
+                    ))}
+            </div>
+        </>
     );
 };
 
